3ac5a644f50c54c600d81517b6d55899f0190727
Dr. Dillinger <peter.dillinger@srh.de>
2023-09-22 11:29:32 +0200

    Jetzt machen wir eine Abfrage von Benutzer. Er soll den Namen eingaben, welchen wir dann wieder ausgeben.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo!");
+        Scanner eingabe = new Scanner(System.in);
+        System.out.println("Hallo, wer bist du: ");
+        String name = eingabe.nextLine();
+        System.out.println("Hallo, " + name);
     }
 }
