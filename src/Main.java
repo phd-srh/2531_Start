ed1eec115ee2edb31536a5e29b3c40d5dd06b832
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-11 14:02:26 +0200

    Eine while-Schleife gebaut, um zählen zu könnne. Die Variable "teiler" ist der Schleifenindex.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,15 @@ public class Main {
         System.out.print("Bitte eine Zahl eingeben: ");
         int zahl = eingabe.nextInt();
 
+        int teiler = 2;
+        while ( teiler <= zahl-1 ) {
+            // was auch immer hier passiert
+            System.out.println(teiler);
+
+            teiler = teiler + 1;
+        }
+
+
         System.out.println(zahl + " ist eine Primzahl");
         System.out.println(zahl + " ist KEINE Primzahl");
     }
