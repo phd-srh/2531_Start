6ac3b5c41efde38644481ae23278fde663119040
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-12 16:00:51 +0200

    Ein paar kleine Spielereien mit Schleifen (eigentlich nur mit der for-Schleife). Hat nix mit der eigentlichen Aufgabe zu tun.

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,31 @@ public class Main {
 //            teiler = teiler + 1;
 //        }
 
+        // AUFGABE: Schleife bauen, das die Zahlen 1 bis 10 ausgibt
+//        for ( int i = 1 ; i <= 10 ; i++ ) {
+//            System.out.println(i);
+//        }
+
+        // AUFGABE: Schleife bauen, das die Zahlen 7 bis 3 ausgibt (absteigend)
+//        for ( int i = 7 ; i >= 3 ; i-- ) {
+//            System.out.println(i);
+//        }
+
+        // AUFGABE: Schleife bauen, das die geraden Zahlen von -5 bis 5 ausgibt
+//        for ( int i = -4 ; i <= 4 ; i += 2 ) {
+//            System.out.println(i);
+//        }
+
+        // AUFGABE: Schleife bauen, das die 7er Reihe von 7 bis 70 ausgibt
+//        for ( int i = 1 ; i <= 10 ; ++i ) {
+//            System.out.println( i*7 );
+//        }
+
+
         // Zählschleife mit merkwürdiger Syntax
-        for ( int teiler = 2 ; teiler <= zahl-1 ; teiler = teiler + 1 ) {
-            System.out.println(teiler);
-        }
+//        for ( int teiler = 2 ; teiler <= zahl-1 ; teiler = teiler + 1 ) {
+//            System.out.println(teiler);
+//        }
 
 
         System.out.println(zahl + " ist eine Primzahl");
